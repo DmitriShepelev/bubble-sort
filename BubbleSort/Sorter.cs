61571011374a65869c334f3906e3ab4a22827edd
dmitri_shepelev <shepelev.dm@gmail.com>
2020-12-23 23:15:43 +0300

    Done

--- BubbleSort/Sorter.cs
+++ BubbleSort/Sorter.cs
@@ -2,6 +2,7 @@
 
 // ReSharper disable InconsistentNaming
 #pragma warning disable SA1611
+#pragma warning disable CA1062
 
 namespace BubbleSort
 {
@@ -12,8 +13,22 @@ namespace BubbleSort
         /// </summary>
         public static void BubbleSort(this int[] array)
         {
-            // TODO #1. Implement the method using a loop statements.
-            throw new NotImplementedException();
+            // #1. Implement the method using a loop statements.
+            if (array is null)
+            {
+                throw new ArgumentNullException($"Array cannot be null.");
+            }
+
+            for (int i = 1; i < array.Length; i++)
+            {
+                for (int j = 0; j < array.Length - i; j++)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        Swap(ref array[j], ref array[j + 1]);
+                    }
+                }
+            }
         }
 
         /// <summary>
@@ -21,8 +36,43 @@ namespace BubbleSort
         /// </summary>
         public static void RecursiveBubbleSort(this int[] array)
         {
-            // TODO #2. Implement the method using recursion algorithm.
-            throw new NotImplementedException();
+            // #2. Implement the method using recursion algorithm.
+            if (array is null)
+            {
+                throw new ArgumentNullException($"Array cannot be null.");
+            }
+
+            if (array.Length == 0)
+            {
+                return;
+            }
+
+            RecursiveBubbleSort(array, array.Length);
+        }
+
+        public static void RecursiveBubbleSort(this int[] array, int length)
+        {
+            if (length == 1)
+            {
+                return;
+            }
+
+            for (int j = 0; j < length - 1; j++)
+            {
+                if (array[j] > array[j + 1])
+                {
+                    Swap(ref array[j], ref array[j + 1]);
+                }
+            }
+
+            RecursiveBubbleSort(array, length - 1);
+        }
+
+        private static void Swap(ref int i, ref int j)
+        {
+            int tmp = i;
+            i = j;
+            j = tmp;
         }
     }
 }
